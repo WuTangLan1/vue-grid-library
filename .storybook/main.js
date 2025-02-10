80bafb0d80565bb28a4cc331cc7186f4a349f5e7
WuTangLan1 <finnmassari16@gmail.com>
2025-02-10 12:17:01 +0200

    still messed up storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,13 @@
-// .storybook\main.js
-
+// .storybook/main.js
 module.exports = {
-    stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: [
-      '@storybook/addon-links',
-      '@storybook/addon-essentials',
-      '@storybook/addon-interactions'
-    ],
-    framework: {
-      name: '@storybook/vue3-vite', 
-      options: {}
-    },
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
+  ],
+  framework: '@storybook/vue3', 
+  core: {
+    builder: '@storybook/builder-vite' 
   }
-  
+};
