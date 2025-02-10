bcd200b8e8f76d648d28f94ea309d57967c7978c
WuTangLan1 <finnmassari16@gmail.com>
2025-02-10 09:54:10 +0200

    added storybook preview

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,1 +1,15 @@
 // storybook\preview.js
+
+import { app } from '@storybook/vue3';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: { expanded: true },
+};
+
+export const decorators = [
+  (story) => ({
+    components: { story },
+    template: '<div style="padding: 20px;"><story /></div>',
+  }),
+];
