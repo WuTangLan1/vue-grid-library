a581210e7239e0facc6068ea636cc849114b521d
WuTangLan1 <finnmassari16@gmail.com>
2025-02-10 12:02:56 +0200

    updating storybook specs

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,8 @@
+// e2e\vue.spec.ts
+
 import { test, expect } from '@playwright/test';
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('h1')).toHaveText('You did it!');
-})
+  await expect(page.locator('h1')).toHaveText('Testing GridBase Component');
+});
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,1 +1,9 @@
 // tests\unit\example.spec.ts
+
+import { describe, it, expect } from 'vitest';
+
+describe('example.spec.ts', () => {
+  it('placeholder test', () => {
+    expect(true).toBe(true);
+  });
+});
